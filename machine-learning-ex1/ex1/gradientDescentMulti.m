3e3106eb95f4566651cc50edf991f5b5545b134c
Anees Hashmi <aneesurhashmi@gmail.com>
2020-04-12 01:13:57 +0500

    Week 4 done

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,7 +16,7 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-	h = sum(theta'.*X,2) % multiply theta with respective feature and then add rows
+	h = sum(theta'.*X,2); % multiply theta with respective feature and then add rows
 	theta = theta - alpha*(1/m)*sum((h-y).*X)';
 
     % ============================================================
